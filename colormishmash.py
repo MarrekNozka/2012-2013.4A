b01ed48354f38dc8cc60789fddb55918c3b841de
Tlapička <marek@tlapicka.net>
2012-11-13 13:54:40 +0100

    colormishmash první verze Út 13.11.2012 13:54:40

---
+++ colormishmash.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# Soubor:  colormishmash.py
+# Datum:   13.11.2012 13:01
+# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
+# Licence: GNU/GPL 
+# Úloha:   jednoduchý program pro míchání barev
+####################################################
+
+import sys
+from Tkinter import *
+####################################################
+
+# ukončí program
+def exit(event=None):
+    sys.exit(0)
+
+
+# formátovaání: http://docs.python.org/2.7/library/string.html#formatstrings
+# převede hodnoty posuvníků na barvu
+def goo(value):
+    color = "#{:02x}{:02x}{:02x}".format(scaleR.get(),scaleG.get(),scaleB.get())
+    colorPanel.config(bg=color)
+
+####################################################
+
+mainWin = Tk()  # vytvořím okno
+mainWin.title("colormishmash.py") # nastavím titulek
+
+# vytvořím udělátko
+scaleR = Scale(mainWin, from_=0, to=255, orient=HORIZONTAL, length=200, 
+               label='červená', command=goo)   
+scaleR.pack()  # umístím udělátko
+scaleG = Scale(mainWin, from_=0, to=255, orient=HORIZONTAL, length=200,
+               label='zelená', command=goo)   
+scaleG.pack()  
+scaleB = Scale(mainWin, from_=0, to=255, orient=HORIZONTAL, length=200, 
+               label='modrá', command=goo)   
+scaleB.pack()  
+ 
+colorPanel = Canvas(mainWin, bg='red', height=200, width=200)
+colorPanel.pack()
+
+mainWin.mainloop()
+
